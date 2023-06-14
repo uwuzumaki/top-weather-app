bea398b30e13553edb6ee343d3aca99644b72ea2
Kris Wong <krischwong@gmail.com>
2023-06-14 01:18:49 -0400

    Added event listener for enter key, display weather function, and styling for input element

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+const displayWeather = (data) => {
+  const container = document.getElementById("container");
+  const current = data.current;
+  const location = data.location;
+  console.log(current, location);
+
+  const oldWeather = document.getElementById("weather");
+  if (oldWeather) {
+    oldWeather.remove();
+  }
+
+  const main = document.createElement("div");
+  main.id = "weather";
+  container.appendChild(main);
+
+  const place = document.createElement("div");
+  place.id = "place";
+  place.textContent = location.name;
+  main.appendChild(place);
+
+  const forecast = document.createElement("div");
+  forecast.id = "forecast";
+  forecast.textContent = current.feelslike_c;
+  main.appendChild(forecast);
+};
+
 const queryLocation = async () => {
   const searchBar = document.getElementById("search-bar");
   const location = searchBar.value;
@@ -7,7 +33,7 @@ const queryLocation = async () => {
       { mode: "cors" }
     );
     const data = await response.json();
-    console.log(data);
+    displayWeather(data);
   } catch (error) {
     console.log(error);
   }
@@ -15,3 +41,9 @@ const queryLocation = async () => {
 
 const searchBarButton = document.getElementById("search-bar-button");
 searchBarButton.addEventListener("click", queryLocation);
+const searchBar = document.getElementById("search-bar");
+searchBar.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    queryLocation();
+  }
+});
